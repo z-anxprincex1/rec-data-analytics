7a2a2343eac572cba3ada7e0ebd84e51baa80e58
saimudunuri9 <anxprince@outlook.com>
2022-09-19 01:06:56 +0530

    r-matrix-basics

--- R Programming/matrices_intro.R
+++ R Programming/matrices_intro.R
@@ -5,9 +5,9 @@ msft <- c(343, 533, 467)
 
 stocks <- c(goog, msft)
 
-stocks.matrix <- matrix(data=stocks,
-                        nrow=2,
-                        byrow=T)
+stocks.matrix <- matrix(data = stocks,
+                        nrow = 2,
+                        byrow = T)
 
 days <- c('SAT', 'SUN', 'MON')
 st.names <- c('GOOG', 'MSFT')
---
+++ R Programming/matrix_arithmetic.R
@@ -0,0 +1,13 @@
+x <- matrix(data = 1:25,
+            nrow = 5,
+            byrow = T)
+
+a <- x * 2
+
+b <- x ^ 2
+
+c <- x + 2
+
+d <- x - 2
+
+e <- x / x
